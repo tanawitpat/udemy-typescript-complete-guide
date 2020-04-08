ef51e0ce0c5f28a7eab079f0a87acc3006fd1b10
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2020-04-08 23:45:46 +0700

    Section 14, Lecture 221

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hi there");
+import express from "express";
+import { router } from "./routes/loginRoutes";
+import bodyParser from "body-parser";
+import cookieSession from "cookie-session";
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ["abcd"] }));
+app.use(router);
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
